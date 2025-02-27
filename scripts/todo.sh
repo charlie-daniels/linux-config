39ebf33f50c76a48e808fed483de574dc3c6b414
charlie-daniels <charlied030401@gmail.com>
2025-02-27 22:21:23 +0000

    add list command

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOGFILE="$HOME/.todo"
+
 helpFunction() {
 	echo "Usage: todo <option> <item>"
 	echo -e "\t-l list all todo items"
@@ -8,12 +10,32 @@ helpFunction() {
 	echo -e "\t-h show this help menu"
 }
 
+checkLog() {
+	if [ ! -f "$LOGFILE" ]; then
+		echo "Creating log file..."
+		touch ~/.todo
+	fi
+}
+
+getList() {
+	if [ ! -s "$LOGFILE" ]; then
+		echo "No entries found."
+		return 0;
+	fi
+	# seperate number and task id
+	while IFS='=' read -r id task; do
+		echo "$id - $task"
+	done < "$LOGFILE"
+}
+
 OPTSTRING=":lrah"
+
+checkLog
 
 while getopts ${OPTSTRING} opt; do
 	case ${opt} in
 		l)
-			echo "list triggered";;
+			getList;;
 		r)
 			echo "remove triggered";;
 		a)
